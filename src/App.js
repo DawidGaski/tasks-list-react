8e38d696056b681521eb364241daa235973ce22f
Dawid Galeski <dawidgaski@gmail.com>
2024-04-25 16:17:50 +0200

    added Navigation component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 import Author from "./features/author/author";
 import TasksPage from "./features/tasks/TasksPage/index";
 import TaskPage from "./features/tasks/TaskPage/index";
+import Navigation from "./common/Navigation";
 import {
   HashRouter,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom/cjs/react-router-dom.min";
-import { StyledNavLink } from "./styled";
 
 export default () => (
   <HashRouter>
-    <nav>
-      <ul>
-        <li>
-          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/autor">O Autorze</StyledNavLink>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/zadania/:id">
-          <TaskPage />
-        </Route>
-        <Route path="/zadania">
-          <TasksPage />
-        </Route>
-        <Route path="/autor">
-          <Author />
-        </Route>
-        <Route path="/">
-          <Redirect to="/zadania" />
-        </Route>
-      </Switch>
-    </nav>
+    <Navigation />
+    <Switch>
+      <Route path="/zadania/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/autor">
+        <Author />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
   </HashRouter>
 );
---
+++ src/common/Navigation/index.js
@@ -0,0 +1,18 @@
+import { toAuthor, toTasks } from "../../routes";
+import { StyledNavList, StyledNavListItem, StyledNavLink } from "./styled";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <StyledNavList>
+        <StyledNavListItem>
+          <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
+        </StyledNavListItem>
+        <StyledNavListItem>
+          <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
+        </StyledNavListItem>
+      </StyledNavList>
+    </nav>
+  );
+};
+export default Navigation;
